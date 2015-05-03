bce1e8ffbadcea3b3d7fd8afcb3a7d5225061abf
Jesse Vander Does <jesse@htmlfusion.com>
2015-05-03 11:55:44 -0700

    Switching back to port 5000

--- linuxtrack_abs.c
+++ linuxtrack_abs.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(5001); 
+  serv_addr.sin_port = htons(5000); 
 
   bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
